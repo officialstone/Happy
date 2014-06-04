e463504b99f65a3cd44bdc9f6ce797170dc8fbe4
officialstone <win8@win8-64.(none)>
2014-06-04 21:20:47 +0700

    v Happy20

--- src/main.cpp
+++ src/main.cpp
@@ -2119,7 +2119,7 @@ bool CBlock::AcceptBlock()
     int nHeight = pindexPrev->nHeight+1;
 
 
-    if (IsProofOfWork() && nHeight > POW_CUTOFF_HEIGHT)
+    if (IsProofOfWork() && nHeight > CUTOFF_HEIGHT)
              return DoS(100, error("AcceptBlock() : No proof-of-work allowed anymore (height = %d)", nHeight));
 
     // Check proof-of-work or proof-of-stake
